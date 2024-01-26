137be29611164723351ae7cda4f536084f54c6c4
anujbhatt-dev <anujbhatt023@gmail.com>
2024-01-26 17:41:38 +0530

    fix: warning

--- dbConnect.js
+++ dbConnect.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI_REMOTE,{
-    useNewUrlParser:true
-}).then(()=>{
+mongoose.connect(process.env.MONGODB_URI_REMOTE).then(()=>{
     console.log("db connected successfully");
 }).catch((e)=>{
     console.log(e);
